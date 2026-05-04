64f12329fbee3e2cd5752f9412f72bb5719be480
maximiliano <maximilianofallini@gmail.com>
2026-05-04 09:39:42 -0300

    mensajes en db

---
+++ models/Mensaje.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const esquemaDeProducto = new mongoose.Schema({
+    autorId: String,
+    nombre: String,
+    texto: String,
+    hora: String,
+    fecha: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const Mensaje = mongoose.model("Mensaje", esquemaDeProducto);
